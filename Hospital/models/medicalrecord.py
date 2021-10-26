b0c2233991930800c4add5b96705f6db0658eddc
Nguyễn Đăng Chiến <ndchien117@gmail.com>
2021-10-26 23:13:01 +0700

    Update medicalrecord.py

--- Hospital/models/medicalrecord.py
+++ Hospital/models/medicalrecord.py
@@ -4,69 +4,55 @@ import datetime
 from odoo import api, fields, models, _
 
 
-class InventoryGoods(models.Model):
+class MedicalRecord(models.Model):
     _name = "medical.record"
-    _description = "Medical Record"
     code = fields.Char(string='HSBA', required=True, copy=False, readonly=True,
                        default=lambda seft: _('New'))
-    patient_name = fields.Char(string='Patient Name', required=True, translate=True)
-    bdate = fields.Date(string='Date of birth', required=True)
-    patient_age = fields.Char(string='Age', compute="_get_age_from_patient")
+    code_bn = fields.Char(string='Mã Bệnh Nhân', required=True)
+    patient_name = fields.Char(string='Tên Bệnh Nhân', required=True, translate=True)
+    bdate = fields.Date(string='Ngày Sinh', required=True)
+    patient_age = fields.Char(string='Tuổi', compute="_get_age_from_patient")
     # gender = fields.Selection([
     #     ('male', 'Male'),
     #     ('female', 'Female'),
     # ], default='male')
-    gender = fields.Selection([('male', 'Male'),
-                              ('female', 'Female')], default='male')
-    phone = fields.Char(string='Phone', required=True)
-    job = fields.Char(string='Job', required=True, translate=True)
-    nation = fields.Char(string='Nation', required=True, translate=True)
-    address = fields.Char(string='Address', required=True, translate=True)
-    workplace = fields.Char(string='Workplace', required=True, translate=True)
-    object = fields.Selection([
-        ('insurance', 'Health Insurance'),
-        ('fee', 'Fee'),
-        ('exempt', 'Exempt'),
-        ('other', 'Other'),
-    ], default='insurance')
-    code_object = fields.Char(string='Code BHYT', required=True)
-    place = fields.Text('PLace')
-    start_date = fields.Date(string='Start Date', help="Enter the start date of health insurance.")
-    end_date = fields.Date(string='End Date', help="Enter the end date of health insurance.")
-    cmnd = fields.Char(string='CMND', required=True)
-    height = fields.Char('Height', required=True)
-    weight = fields.Char('Weight', required=True)
-    blood_group = fields.Selection([
-        ('a', 'A'),
-        ('b', 'B'),
-        ('o', 'O'),
-        ('ab', 'AB')
-    ], string='Blood Group', default='a')
-    note = fields.Text(string='Description of contact information')
-    start_date_hospital = fields.Date(string='Hospitalized Day', help="Enter the hospital admission date.")
-    end_date_hospital = fields.Date(string='Hospital Discharge Date', help="Enter the hospital discharge date.")
-    department = fields.Char(string='Department', required=True)
-    hospital_room = fields.Char(string='Hospital Room', required=True)
-    hospital_bed = fields.Char(string='Hospital Bed', required=True)
-    doctor = fields.Char(string='Doctor', required=True)
-    nurse = fields.Char(string='Nurse')
-    patient_test = fields.Char(string='Patient Test', required=True)
+    gender = fields.Selection([('male', 'Nam'),
+                               ('female', 'Nữ')], default='male')
+    job = fields.Char(string='Nghề Nghiệp', required=True, translate=True)
+    address = fields.Char(string='Địa Chỉ', required=True, translate=True)
+    workplace = fields.Char(string='Nơi Làm Việc', required=True, translate=True)
+    object_bn = fields.Char(string='Đối Tượng', required=True)
+    start_date = fields.Date(string='Ngày Khám', help="Vui Lòng chọn thời gian ngày khám.")
+    end_date = fields.Date(string='Ngày Kết Thúc', help="Vui Lòng chọn thời gian ngày kết thúc.")
+    note = fields.Text(string='Kết Luận')
+    diagnostic = fields.Char(string='Chuẩn Đoán', required=True)
+    treatment = fields.Selection([
+        ('nhapvien', 'Nhập Viện Điều Trị'),
+        ('capcuu', 'Cấp Cứu Khẩn Cấp'),
+        ('kedon', 'Kê Đơn Thuốc')
+    ], string='Hướng Điều Trị', default='nhapvien')
+    banthan = fields.Char('Bản Thân', required=True)
+    family = fields.Char('Gia Đình', required=True)
+    diungthuoc = fields.Boolean(string='Dị Ứng Thuốc')
+    start_date_hospital = fields.Date(string='Ngày Khám')
+    doctor = fields.Char(string='Bác Sỹ', required=True)
+    qtbly = fields.Char(string='Quá Trình Bệnh Lý', required=True)
+    kham = fields.Char(string='Khám Lâm Sàng', required=True)
+    heart = fields.Char(string='Nhịp Tim', required=True)
+    temperature = fields.Char(string='Nhiệt Độ', required=True)
+    huyetap = fields.Char(string='Huyết Áp', required=True)
+    nhiptho = fields.Char(string='Nhịp Thở', required=True)
+    cannang = fields.Char(string='Cân Nặng', required=True)
+    chieucao = fields.Char(string='Chiều Cao', required=True)
+    benhkem = fields.Char(string='Bệnh Kèm Theo', required=True)
+    note_sick = fields.Text(string='Mô Tả Kết Luận')
     image = fields.Binary(string='Images')
-    treatments = fields.Char(string='Treatments', required=True)
-    result = fields.Selection([
-        ('cured', 'Cured'),
-        ('better', 'Better'),
-        ('constant', 'Constant'),
-        ('sicker', 'Get Sicker'),
-        ('dead', 'Dead'),
-    ], default='cured')
-    note_sick = fields.Text(string='Description of illness information')
 
     @api.model
     def create(self, vals):
         if vals.get('code', ('New')) == ('New'):
             vals['code'] = self.env['ir.sequence'].next_by_code('medical.record') or _('New')
-        res = super(InventoryGoods, self).create(vals)
+        res = super(MedicalRecord, self).create(vals)
         return res
 
     def _get_age_from_patient(self):
