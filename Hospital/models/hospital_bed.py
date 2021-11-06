cfb773445cfb2463df831b07a7d52d0e9516ff9f
trinhtran02 <181121521238@due.udn.vn>
2021-11-06 07:50:17 +0700

    update

--- Hospital/models/hospital_bed.py
+++ Hospital/models/hospital_bed.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+import datetime
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+
 
 class HospitalBed(models.Model):
     _inherit = "product.template"
@@ -15,6 +17,10 @@ class HospitalBed(models.Model):
         ('trong', 'Trống'),
         ('day', 'Đầy')
     ], string='Trạng thái', required=True, default='trong')
+    loai = fields.Selection([
+        ('coban', 'Giường y tế cơ bản'),
+        ('dachucnang', 'Giường đa chức năng')
+    ], string='Loại giường', default='coban', required=True)
     chieudai = fields.Char('Chiều dài', required=True)
     chieurong = fields.Char('Chiều rộng', required=True)
     chieucao = fields.Char('Chiều cao', required=True)
@@ -28,15 +34,36 @@ class HospitalBed(models.Model):
         ('nam', 'Nam'),
         ('nu', 'Nữ')
     ], string='Giới tính', required=True, default='nam')
-
     chuandoan = fields.Char('Chuẩn đoán bệnh', required=True)
     thoigian_nhapvien = fields.Date(string='Thời gian nhập viện', help="Nhập thời gian bắt đầu nhập viện.")
-    songay_nhapvien = fields.Integer('Số ngày nhập viện', required=True)
+    songay_nhapvien = fields.Char(string='Số ngày nhập viện', compute="_get_date")
     bacsi_phutrach = fields.Char('Bác sĩ phụ trách', required=True)
     phongbenh = fields.Char('Tên phòng', required=True)
     note = fields.Text('Ghi chú:', required=True)
-    loai = fields.Selection([
-        ('coban', 'Giường y tế cơ bản'),
-        ('dachucnang', 'Giường đa chức năng')
-    ], string='Loại giường', default='coban', required=True)
 
+    @api.constrains('id_bed')
+    def _check_id_bed(self):
+        exists = self.env['product.template'].search(
+            [('id_bed', '=', self.id_bed), ('id', '!=', self.id)])
+        if exists:
+            raise ValidationError(_('Mã giường  ' + self.id_bed + ' đã tồn tại.'))
+
+    def copy(self, default={}):
+        if self.id_bed == False:
+            default['id_bed'] = str(self.id) + " (Bản sao)"
+        else:
+            default['id_bed'] = self.id_bed + " (Bản sao)"
+
+        res = super(HospitalBed, self).copy(default=default)
+        return res
+
+    @api.model
+    def _get_date(self):
+        today_date = datetime.date.today()
+        for bed in self:
+            if bed.thoigian_nhapvien:
+                thoigian_nhapvien = fields.Datetime.to_datetime(bed.thoigian_nhapvien).date()
+                dem = str(int((today_date - thoigian_nhapvien).days))
+                bed.songay_nhapvien = dem
+            else:
+                bed.songay_nhapvien = ""
